c5aecfa7ce2282c1bf51f428fa8cda6e58093faf
yo-huaca95 <yo.huaca@udla.edu.co>
2023-05-06 22:32:43 -0500

    Ajuste de Registro de Productos

--- Actividad-1-Front/src/Components/MenuNavegacionIzquierdo/OpcionesMenu/RegistroProductos/RegistroProductos.js
+++ Actividad-1-Front/src/Components/MenuNavegacionIzquierdo/OpcionesMenu/RegistroProductos/RegistroProductos.js
@@ -1,13 +1,16 @@
 import React from "react";
-import {Form} from "react-bootstrap";
+import {Container, Form, Row} from "react-bootstrap";
+import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 
 export  const RegistroProductos= () => {
     return (
+      <Container>
+      <Row>
       <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Label>Producto</Form.Label>
+        <Form.Control type="text" placeholder="Enter email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -24,5 +27,38 @@ export  const RegistroProductos= () => {
         Submit
       </Button>
     </Form>
+    </Row>
+    <Row>
+    <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+    </Row>
+    </Container>
     );
 }
--- Actividad-1-Front/src/Components/MenuNavegacionIzquierdo/OpcionesMenu/Ventas/Ventas.js
+++ Actividad-1-Front/src/Components/MenuNavegacionIzquierdo/OpcionesMenu/Ventas/Ventas.js
@@ -20,8 +20,8 @@ export  const Ventas= () => {
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
+      <Button variant="primary" >
+        Registrar
       </Button>
     </Form>
     );
