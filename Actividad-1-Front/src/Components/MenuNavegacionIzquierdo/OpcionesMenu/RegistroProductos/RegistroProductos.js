8ff3fa045fa1eb90e81aa0f8c8ebeeae064b102e
yo-huaca95 <yo.huaca@udla.edu.co>
2023-05-10 11:53:16 -0500

    avance tabla 4

--- Actividad-1-Front/src/Components/MenuNavegacionIzquierdo/OpcionesMenu/RegistroProductos/RegistroProductos.js
+++ Actividad-1-Front/src/Components/MenuNavegacionIzquierdo/OpcionesMenu/RegistroProductos/RegistroProductos.js
@@ -5,48 +5,56 @@ import Button from 'react-bootstrap/Button';
 import './RegistroProductos.css';
 import {Tabla} from './Tabla/Tabla'
 
+
+
+
+
 export  const RegistroProductos= () => {
 
     return (
-      <Container className="conta">
-      <Row>
-      <Col >
-      <Card>
+      <Container >
+      <Row >
+      <Col md={{ span: 5 }}>
+      <Card className="ajusteDimencion" >
       <Card.Header className="TituloComponentes" ><span className="spanLetraTextoTitulo">Registrar Producto</span></Card.Header>
         <Card.Body>
       <Form >
-      <Form.Group as={Row} className="mb-3" controlId="formBasicEmail">
+      <Form.Group as={Row} className="mb-3">
         <Form.Label column md={{ span: 3  }} ><span className="spanletraTextoNormal" >Producto</span></Form.Label>
         <Col md={{ span: 9  }}>
         <Form.Control  type="text" placeholder="Ingrese producto" />
         </Col>
       </Form.Group>
 
-      <Form.Group as={Row}  className="mb-3" controlId="formBasicPassword">
+      <Form.Group as={Row}  className="mb-3" >
         <Form.Label column md={{ span: 3  }}><span className="spanletraTextoNormal">Precio</span> </Form.Label>
         <Col md={{ span: 9  }}>
         <Form.Control type="number" placeholder="Precio del producto" min={0} />
         </Col>
       </Form.Group>
-      <Button variant="primary" >
-        Crear
+      <Form.Group as={Row}  className="mb-3">
+        <Form.Label column md={{ span: 3  }}><span className="spanletraTextoNormal">Lote</span> </Form.Label>
+        <Col md={{ span: 9  }}>
+        <Form.Control type="number" placeholder="Lote del producto" min={0} />
+        </Col>
+      </Form.Group>
+      <Button size="sm" variant="primary"  >
+        <span className="spanletraTextoNormal btnspan">Crear</span>
       </Button>
     </Form>
     </Card.Body>
     </Card>
 
     </Col>
-  <Col >
+  <Col md={{ span: 7 }} >
   <Card  className="CardsTablePrRegistrar">
       <Card.Header className="TituloComponentes"><span className="spanLetraTextoTitulo">Productos Registrados</span></Card.Header>
         <Card.Body>
-      <Container >
        <Tabla/>
-     </Container>
     </Card.Body>
     </Card>
     </Col>
     </Row>
-    </Container>
+      </Container>
     );
 }
